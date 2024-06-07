6e0e931fb576b4185d93ae33a590d45453994249
Isha Tiwari <ishatiwari406@gmail.com>
2024-06-07 11:05:22 +0530

    # while loop

--- login.py
+++ login.py
@@ -1,6 +1,6 @@
 while True:
-    username=input("Enter the username: ")
-    passward=input("Enter the passward: ")
+    username=input("Enter the username:  ")
+    passward=input("Enter the passward:  ")
     if username=='admin' and passward=='1234':
         print("login successful!")
         break
