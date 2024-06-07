5dd06d45d937c456a00fdc0a7fe7015665f105b9
Isha Tiwari <ishatiwari406@gmail.com>
2024-06-07 11:00:33 +0530

    # strings

---
+++ login.py
@@ -0,0 +1,8 @@
+while True:
+    username=input("Enter the username: ")
+    passward=input("Enter the passward: ")
+    if username=='admin' and passward=='1234':
+        print("login successful!")
+        break
+    else:
+        print("login failed!")
---
+++ story.py
@@ -0,0 +1,10 @@
+kahani=''
+while True:
+    line=input(">>>")
+    if not line:
+        break
+    else:
+        kahani+=line+'\n'
+print('The story is:\n')
+print(kahani)
+print("⭐⭐⭐⭐⭐")
